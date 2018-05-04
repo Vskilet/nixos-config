5faed22f4ab28c075499371b8aa8b28255da2b05
SENNAS01 <Victor@SENNAS01>
2018-05-04 14:31:07 +0200

    add opcache support

--- nextcloud.nix
+++ nextcloud.nix
@@ -120,6 +120,19 @@ in
         pm.max_requests = 500
       '';
     };
+    
+    services.phpfpm.phpOptions = 
+      ''
+        
+        zend_extension=opcache.so 
+        opcache.enable=1
+        opcache.enable_cli=1
+        opcache.interned_strings_buffer=8
+        opcache.max_accelerated_files=10000
+        opcache.memory_consumption=128
+        opcache.save_comments=1
+        opcache.revalidate_freq=1
+      '';
 
     users.extraUsers.nginx.extraGroups = [ cfg.phpfpm.group ];
 
