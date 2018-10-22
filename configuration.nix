217dc2bcd58bd28e4506f0fad77fa2d835faf23a
Victor SENE <victor@sene.ovh>
2018-10-22 13:40:03 +0200

    [18.09] change nixos zsh options

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
   # started in user sessions.
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
-  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.autosuggestions.enable = true;
   programs.zsh.enableCompletion = true;
   programs.zsh.syntaxHighlighting.enable = true;
   programs.zsh.ohMyZsh.enable = true;
