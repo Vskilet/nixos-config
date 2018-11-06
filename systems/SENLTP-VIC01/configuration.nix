7bf630a509b83eedb27b9d7e7c343017b5f71fcc
Victor SENE <victor@sene.ovh>
2018-11-06 19:17:50 +0100

    add tlp system configuration

--- systems/SENLTP-VIC01/configuration.nix
+++ systems/SENLTP-VIC01/configuration.nix
@@ -96,6 +96,13 @@
   virtualisation.virtualbox.host.enableExtensionPack = true;
 
   services.tlp.enable = true;
+  services.tlp.extraConfig = ''
+    DEVICES_TO_DISABLE_ON_STARTUP="bluetooth"
+    START_CHARGE_THRESH_BAT0=85
+    STOP_CHARGE_THRESH_BAT0=90
+    CPU_SCALING_GOVERNOR_ON_BAT=powersave
+    ENERGY_PERF_POLICY_ON_BAT=powersave
+  '';
   services.sshd.enable = true; 
   services.pcscd.enable = true;
 
