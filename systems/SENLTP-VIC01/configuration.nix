6a499c051122a12f14a6011380646a538901480f
Victor SENE <victor@sene.ovh>
2018-11-06 19:17:05 +0100

    add some software

--- systems/SENLTP-VIC01/configuration.nix
+++ systems/SENLTP-VIC01/configuration.nix
@@ -56,6 +56,7 @@
     })
     imagemagick
     pciutils
+    usbutils
     git
     tig
     gnome-breeze
@@ -65,6 +66,7 @@
     libreoffice
     gimp
     vlc
+    nextcloud-client
     spotify
   ];
   programs.wireshark.enable = true;
