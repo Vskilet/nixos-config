8f94a17f0551448dd6647b2df93949337d27cbe9
Victor SENE <victor@sene.ovh>
2019-02-05 15:08:22 +0100

    refactor prompt organization

--- configuration/motd.sh
+++ configuration/motd.sh
@@ -1,11 +1,9 @@
-#!/run/current-system/sw/bin/bash 
+#!/run/current-system/sw/bin/bash
 
 USER=`whoami`
 HOSTNAME=`uname -n`
 
 LINUX=`uname -rs`
-
-DISK=`df -m | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3/1000,$2/1000,$5}'`
 
 MEMORY=`free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'`
 
@@ -13,9 +11,9 @@ MEMORY=`free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3
 HOUR=$(date +"%H")
 if [ $HOUR -lt 12  -a $HOUR -ge 0 ]
 then    TIME="morning"
-elif [ $HOUR -lt 17 -a $HOUR -ge 12 ] 
+elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]
 then    TIME="afternoon"
-else 
+else
     TIME="evening"
 fi
 
@@ -34,41 +32,36 @@ LOAD15=`cat /proc/loadavg | awk {'print $3'}`
 IPV4=`dig +short sene.ovh @resolver1.opendns.com`
 IPV6=`ip -6 addr show enp2s0 | awk '/inet6/{print $2}' | cut -d/ -f1 | head -1`
 
-DOCKER=`docker --version | awk -F'[, ]' '{printf "%s",$3} END {print ""}'`
-
+echo "============================================================"
 echo "Good $TIME $USER"
-echo "Here is your server state. Enjoy :)"
-
-echo "
-============================================================
+echo `curl -s wttr.in/Haguenau\?format=3`
+echo "============================================================
  - Hostname............: $HOSTNAME
  - IPv4................: $IPV4
  - IPv6................: $IPV6
  - Release.............: `nixos-version`
  - Kernel..............: $LINUX
- - Users...............: Currently `users | wc -w` user(s) logged on
 ============================================================
+ - Users...............: Currently `users | wc -w` user(s) logged on
  - Current user........: $USER
  - CPU usage...........: $LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)
  - Memory used.........: $MEMORY
  - Swap in use.........: `free -m | tail -n 1 | awk '{print $3}'` MB
  - System uptime.......: $upDays days $upHours hours $upMins minutes $upSecs seconds
- - Disk space ROOT.....: $DISK
-============================================================
- - Docker..............: $DOCKER
-"
-services=( "haproxy.service" "nginx.service" "dovecot2.service" "phpfpm-roundcube.service" "phpfpm-nextcloud.service" "searx.service" "matrix-synapse.service" "grafana.service" "shellinaboxd.service" "emby.service" "transmission.service" )
+============================================================"
+services=( "grafana.service" "searx.service" "gitea.service" "matrix-synapse.service" "docker.service" "" "dovecot2.service" "postfix.service" "rspamd.service" "" "haproxy.service" "nginx.service" "phpfpm-web.service" "phpfpm-roundcube.service" "phpfpm-nextcloud.service" "" "pgmanage.service" "postgresql.service" "mysql.service" "" "emby.service" "transmission.service" "jackett.service" "radarr.service" "sonarr.service" )
 
 for var in "${services[@]}"
 do
-        if systemctl -q is-active ${var}
-        then
-                printf "%-40s [\e[32mOK\e[39m]\n" $var
-        else
-                printf "%-40s [\e[31mFAIL\e[39m]\n" $var
-        fi
+    if [[ -z $var ]]; then
+      printf "\n"
+    else
+      if systemctl -q is-active ${var}; then
+        printf "%-40s [\e[32mOK\e[39m]\n" $var
+      else
+        printf "%-40s [\e[31mFAIL\e[39m]\n" $var
+      fi
+    fi
 done
 
-echo "
-============================================================
-"
+echo "============================================================"
