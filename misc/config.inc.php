d42862cad4d2221dac175d9e7786c9583b8f0038
Victor SENE <victor@sene.ovh>
2019-02-20 16:39:56 +0100

    roundcube: use my module

--- misc/config.inc.php
+++ misc/config.inc.php
@@ -1,137 +1,18 @@
-
-
-/* Local configuration for Roundcube Webmail */
 // ----------------------------------
-// SYSTEM
-// ----------------------------------
-$config['enable_installer'] = false;
-
-// Name your service. This is displayed on the login screen and in the window title
-$config['product_name'] = 'Webmail SENE-NET';
-
-// ----------------------------------
-// SQL DATABASE
-// ----------------------------------
-// Database connection string (DSN) for read+write operations
-// Format (compatible with PEAR MDB2): db_provider://user:password@host/database
-// Currently supported db_providers: mysql, pgsql, sqlite, mssql, sqlsrv, oracle
-// For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
-// NOTE: for SQLite use absolute path (Linux): 'sqlite:////full/path/to/sqlite.db?mode=0646'
-//       or (Windows): 'sqlite:///C:/full/path/to/sqlite.db'
-//$config['db_dsnw'] = 'pgsql://roundcube:roundcube@localhost/roundcube';
-
-// you can define specific table (and sequence) names prefix
-$config['db_prefix'] = 'rc';
-
-// log driver:  'syslog', 'stdout' or 'file'.
-//$config['log_driver'] = 'syslog';
-
-// ----------------------------------
-// IMAP
-// ----------------------------------
-// The IMAP host chosen to perform the log-in.
-// Leave blank to show a textbox at login, give a list of hosts
-// to display a pulldown menu or set one host as string.
-// To use SSL/TLS connection, enter hostname with prefix ssl:// or tls://
-// Supported replacement variables:
-// %n - hostname ($_SERVER['SERVER_NAME'])
-// %t - hostname without the first part
-// %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
-// %s - domain name after the '@' from e-mail address provided at login screen
-// For example %n = mail.domain.tld, %t = domain.tld
-// WARNING: After hostname change update of mail_host column in users table is
-//          required to match old user data records with the new host.
-$config['default_host'] = 'localhost';
-
-// TCP port used for IMAP connections
-//$config['default_port'] = 143;
-
-// IMAP authentication method (DIGEST-MD5, CRAM-MD5, LOGIN, PLAIN or null).
-// Use 'IMAP' to authenticate with IMAP LOGIN command.
-// By default the most secure method (from supported) will be selected.
-//$config['imap_auth_type'] = 'PLAIN';
-
-// ----------------------------------
-// SMTP
-// ----------------------------------
-$config['smtp_server'] = 'tls://sene.ovh';
-
-// SMTP port (default is 587)
-$config['smtp_port'] = 587;
-
-// SMTP username (if required) if you use %u as the username Roundcube
-// will use the current username for login
-$config['smtp_user'] = '%u';
-
-// SMTP password (if required) if you use %p as the password Roundcube
-// will use the current user's password for login
-$config['smtp_pass'] = '%p';
-
-// SMTP AUTH type (DIGEST-MD5, CRAM-MD5, LOGIN, PLAIN or empty to use
-// best server supported one)
-//$config['smtp_auth_type'] = 'PLAIN';
-
-//$config['smtp_conn_options'] = array(
-//     'ssl' => array(
-//       'verify_peer' => true,
-//       // certificate is not self-signed if cafile provided
-//       'allow_self_signed' => false,
-//       // For Letsencrypt use the following two lines and remove the 'cafile' option above.
-//       //'ssl_cert' => '/var/lib/acme/sene.ovh/fullchain.pem'
-//       //'ssl_key'  => '/var/lib/acme/sene.ovh/key.pem'
-//       // probably optional parameters
-//       'ciphers' => 'TLSv1+HIGH:!aNull:@STRENGTH',
-//       'peer_name' => 'mail.sene.ovh',
-//     ),
-// );
-
+// EXTRA CONFIGURATION
 // ----------------------------------
 // provide an URL where a user can get support for this Roundcube installation
 // PLEASE DO NOT LINK TO THE ROUNDCUBE.NET WEBSITE HERE!
 $config['support_url'] = 'https://sene.ovh';
-
-// use this folder to store log files
-// must be writeable for the user who runs PHP process (Apache user if mod_php is being used)
-// This is used by the 'file' log driver.
-$config['log_dir'] = '/tmp/';
-
-// use this folder to store temp files
-// must be writeable for the user who runs PHP process (Apache user if mod_php is being used)
-$config['temp_dir'] = '/tmp/';
 
 // This key is used for encrypting purposes, like storing of imap password
 // in the session. For historical reasons it's called DES_key, but it's used
 // with any configured cipher_method (see below).
 $config['des_key'] = 'JZUH4FTsIUJJE4l6J5CTZeKz';
 
-// Encryption algorithm. You can use any method supported by openssl.
-// Default is set for backward compatibility to DES-EDE3-CBC,
-// but you can choose e.g. AES-256-CBC which we consider a better choice.
-$config['cipher_method'] = 'AES-256-CBC';
-
-// Automatically add this domain to user names for login
-// Only for IMAP servers that require full e-mail addresses for login
-// Specify an array with 'host' => 'domain' values to support multiple hosts
-// Supported replacement variables:
-// %h - user's IMAP hostname
-// %n - hostname ($_SERVER['SERVER_NAME'])
-// %t - hostname without the first part
-// %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
-// %z - IMAP domain (IMAP hostname without the first part)
-// For example %n = mail.domain.tld, %t = domain.tld
-$config['username_domain'] = '%d';
-
-// Message size limit. Note that SMTP server(s) may use a different value.
-// This limit is verified when user attaches files to a composed message.
-// Size in bytes (possible unit suffix: K, M, G)
-//$config['max_message_size'] = '25M';
-
 // ----------------------------------
-// PLUGINS
+// USER PREFERENCES
 // ----------------------------------
-// List of active plugins (in plugins/ directory)
-//$config['plugins'] = array('archive', 'attachment_reminder', 'autologon', 'emoticons', 'enigma', 'filesystem_attachments', 'help', 'identicon', 'identity_select', 'jqueryui', 'managesieve', 'newmail_notifier', 'password', 'show_additional_headers', 'subscriptions_option', 'virtuser_file', 'zipdownload');
-
 // the default locale setting (leave empty for auto-detection)
 // RFC1766 formatted language name like en_US, de_DE, de_CH, fr_FR, pt_BR
 $config['language'] = 'fr_FR';
@@ -158,9 +39,6 @@ $config['mime_param_folding'] = 0;
 // If true all folders will be checked for recent messages
 $config['check_all_folders'] = true;
 
-// ----------------------------------
-// USER PREFERENCES
-// ----------------------------------
 // Use this charset as fallback for message decoding
 $config['default_charset'] = 'UTF-8';
 
@@ -170,10 +48,10 @@ $config['skin'] = 'larry';
 // Default messages listing mode. One of 'threads' or 'list'.
 $config['default_list_mode'] = 'threads';
 
-// prefer displaying HTML messages
+// Prefer displaying HTML messages
 $config['prefer_html'] = true;
 
-// compose html formatted messages by default
+// Compose html formatted messages by default
 //  0 - never,
 //  1 - always,
 //  2 - on reply to HTML message,
