7de3f452bac3348069b5ec82efff028fca45b0f9
Victor SENE <victor@sene.ovh>
2019-09-24 22:58:21 +0200

    riot-web: add widgets support

--- overlays/riot-web.nix
+++ overlays/riot-web.nix
@@ -6,7 +6,11 @@ self: super:
         "default_hs_url": "https://matrix.sene.ovh",
         "default_is_url": "https://vector.im",
         "brand": "SENE-NET",
-        "default_theme": "dark"
+        "default_theme": "dark",
+        "integrations_ui_url": "https://dimension.t2bot.io/riot",
+        "integrations_rest_url": "https://dimension.t2bot.io/api/v1/scalar",
+        "integrations_widgets_urls": ["https://dimension.t2bot.io/widgets"],
+        "integrations_jitsi_widget_url": "https://dimension.t2bot.io/widgets/jitsi"
       }
     '';
   };
