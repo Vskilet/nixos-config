541de9835e03a91fbc35a4d7195b2b33899ed68a
Victor SENE <victor@sene.ovh>
2019-11-04 17:23:55 +0100

    update mailserver

--- services/mailserver.nix
+++ services/mailserver.nix
@@ -21,7 +21,7 @@ in
   imports = [
     (builtins.fetchTarball {
       url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/master/nixos-mailserver-master.tar.gz";
-      sha256 = "196wy23b3s0j2x9zb3kbvwzg29ww3arfr6z8pqdgmampba4npirb";
+      sha256 = "0wn94js60i6v2lm03p5c162dimqcdgsjjjd0638zr4crf852w2nc";
     })
   ];
 
