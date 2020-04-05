95b3ef537c84c4e8f5987f01e6a2f67a2d86ad3e
Victor SENE <victor@sene.ovh>
2020-04-05 18:23:21 +0200

    update riot-web variables and structure

--- overlays/riot-web.nix
+++ overlays/riot-web.nix
@@ -2,14 +2,22 @@ self: super:
 {
   riot-web = super.riot-web.override {
     conf = {
-        default_hs_url = "https://matrix.sene.ovh";
-        default_is_url = "https://vector.im";
-        brand = "SENE-NET";
-        default_theme = "dark";
-        integrations_ui_url = "https://dimension.t2bot.io/riot";
-        integrations_rest_url = "https://dimension.t2bot.io/api/v1/scalar";
-        integrations_widgets_urls = ["https://dimension.t2bot.io/widgets"];
-        integrations_jitsi_widget_url = "https://dimension.t2bot.io/widgets/jitsi";
+      default_server_config = {
+        "m.homeserver" = {
+          base_url = "https://matrix.sene.ovh";
+          server_name = "sene.ovh";
+        };
+        "m.identity_server" = {
+          base_url = "https://vector.im";
+        };
+      };
+      brand = "SENE-NET";
+      default_theme = "dark";
+      defaultCountryCode = "FR";
+      integrations_ui_url = "https://dimension.t2bot.io/riot";
+      integrations_rest_url = "https://dimension.t2bot.io/api/v1/scalar";
+      integrations_widgets_urls = ["https://dimension.t2bot.io/widgets"];
+      integrations_jitsi_widget_url = "https://dimension.t2bot.io/widgets/jitsi";
     };
   };
 }
