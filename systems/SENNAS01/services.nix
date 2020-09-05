d54566b70eccea96561d1d69d8d53e311e421910
Victor SENE <victor@sene.ovh>
2020-09-05 10:56:30 +0200

    use onlyoffice

--- systems/SENNAS01/services.nix
+++ systems/SENNAS01/services.nix
@@ -176,72 +176,12 @@ in
         forceSSL = true;
         locations = { "/" = { root = pkgs.riot-web; }; };
       };
-      "office.sene.ovh" = {
-        enableACME = true;
-        forceSSL = true;
-        extraConfig = ''
-          # static files
-          location ^~ /loleaflet {
-              proxy_pass https://localhost:9980;
-              proxy_set_header Host $host;
-          }
-
-          # WOPI discovery URL
-          location ^~ /hosting/discovery {
-              proxy_pass https://localhost:9980;
-              proxy_set_header Host $host;
-          }
-
-          # main websocket
-          location ~ ^/lool/(.*)/ws$ {
-              proxy_pass https://localhost:9980;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection "Upgrade";
-              proxy_set_header Host $host;
-              proxy_read_timeout 36000s;
-          }
-
-          # download, presentation and image upload
-          location ~ ^/lool {
-              proxy_pass https://localhost:9980;
-              proxy_set_header Host $host;
-          }
-
-          # Admin Console websocket
-          location ^~ /lool/adminws {
-              proxy_pass https://localhost:9980;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection "Upgrade";
-              proxy_set_header Host $host;
-              proxy_read_timeout 36000s;
-          }
-        '';
-      };
       "cloud.sene.ovh" = {
         enableACME = true;
         forceSSL = true;
         serverAliases = [ "cloud.stech.ovh" ];
-        locations = {
-#          "~ ^\/(?:index|remote|public|cron|core\/ajax\/update|status|ocs\/v[12]|updater\/.+|oc[ms]-provider\/.+|.+\/richdocumentscode\/proxy)\.php(?:$|\/)" = {
-           "~ ^\\/(?:index|remote|public|cron|core/ajax\\/update|status|ocs\\/v[12]|updater\\/.+|ocs-provider\\/.+|ocm-provider\\/.+|.+\\/richdocumentscode\\/proxy)\\.php(?:$|\\/)" = {
-            extraConfig = ''
-              include ${config.services.nginx.package}/conf/fastcgi.conf;
-              fastcgi_split_path_info ^(.+\.php)(\\/.*)$;
-              try_files $fastcgi_script_name =404;
-              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-
-              fastcgi_param PATH_INFO $fastcgi_path_info;
-              fastcgi_param HTTPS ${if config.services.nextcloud.https then "on" else "off"};
-              fastcgi_param modHeadersAvailable true;
-              fastcgi_param front_controller_active true;
-              fastcgi_pass unix:${config.services.phpfpm.pools.nextcloud.socket};
-              fastcgi_intercept_errors on;
-              fastcgi_request_buffering off;
-              fastcgi_read_timeout 120s;
-            '';
-          };
-        };
       };
+      "onlyoffice.sene.ovh" = simpleReverse 9981;
       "matrix.sene.ovh" = simpleReverse 8008;
       "searx.sene.ovh" = simpleReverse 8888;
       "git.sene.ovh" = simpleReverse config.services.gitea.httpPort;
