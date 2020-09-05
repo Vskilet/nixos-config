14a40373d69ff199dee453f3e28d05b08d875fd7
Victor SENE <victor@sene.ovh>
2020-09-05 10:50:45 +0200

    collabora office test but not functional

--- systems/SENNAS01/services.nix
+++ systems/SENNAS01/services.nix
@@ -219,6 +219,27 @@ in
       "cloud.sene.ovh" = {
         enableACME = true;
         forceSSL = true;
+        serverAliases = [ "cloud.stech.ovh" ];
+        locations = {
+#          "~ ^\/(?:index|remote|public|cron|core\/ajax\/update|status|ocs\/v[12]|updater\/.+|oc[ms]-provider\/.+|.+\/richdocumentscode\/proxy)\.php(?:$|\/)" = {
+           "~ ^\\/(?:index|remote|public|cron|core/ajax\\/update|status|ocs\\/v[12]|updater\\/.+|ocs-provider\\/.+|ocm-provider\\/.+|.+\\/richdocumentscode\\/proxy)\\.php(?:$|\\/)" = {
+            extraConfig = ''
+              include ${config.services.nginx.package}/conf/fastcgi.conf;
+              fastcgi_split_path_info ^(.+\.php)(\\/.*)$;
+              try_files $fastcgi_script_name =404;
+              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+
+              fastcgi_param PATH_INFO $fastcgi_path_info;
+              fastcgi_param HTTPS ${if config.services.nextcloud.https then "on" else "off"};
+              fastcgi_param modHeadersAvailable true;
+              fastcgi_param front_controller_active true;
+              fastcgi_pass unix:${config.services.phpfpm.pools.nextcloud.socket};
+              fastcgi_intercept_errors on;
+              fastcgi_request_buffering off;
+              fastcgi_read_timeout 120s;
+            '';
+          };
+        };
       };
       "matrix.sene.ovh" = simpleReverse 8008;
       "searx.sene.ovh" = simpleReverse 8888;
@@ -303,6 +324,16 @@ in
       dbtableprefix = "oc_";
       adminpassFile = "/mnt/secrets/nextcloud_config_adminpassFile";
       extraTrustedDomains = config.services.nginx.virtualHosts."cloud.sene.ovh".serverAliases;
+    };
+  };
+
+  services.loolwsd = {
+    enable = true;
+    package = pkgs.collabora-online;
+    dictionaries = [ "fr-moderne" ];
+    proxy = {
+      enable = true;
+      hostname = config.services.nextcloud.hostName;
     };
   };
 
