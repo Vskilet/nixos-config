234d0458cd4905db689ff0615860b1a7f376fb0a
Victor SENE <victor@sene.ovh>
2020-12-11 16:27:21 +0100

    tlp: use settings options instead of extraOptions

--- systems/SENLTP-VIC01/configuration.nix
+++ systems/SENLTP-VIC01/configuration.nix
@@ -132,13 +132,13 @@ in
   };
 
   services.tlp.enable = true;
-  services.tlp.extraConfig = ''
-    DEVICES_TO_DISABLE_ON_STARTUP="bluetooth"
-    START_CHARGE_THRESH_BAT0=85
-    STOP_CHARGE_THRESH_BAT0=95
-    CPU_SCALING_GOVERNOR_ON_BAT=powersave
-    ENERGY_PERF_POLICY_ON_BAT=powersave
-  '';
+  services.tlp.settings = {
+    DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+    START_CHARGE_THRESH_BAT0 = 85;
+    STOP_CHARGE_THRESH_BAT0 = 95;
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    ENERGY_PERF_POLICY_ON_BAT = "powersave";
+  };
 
   services.pcscd.enable = true;
 
