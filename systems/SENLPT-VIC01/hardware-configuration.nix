567e0a572fc33cb8672348df8854533944e9b141
Victor SENE <victor@sene.ovh>
2022-10-22 01:44:43 +0200

    try to fix hostname

--- systems/SENLPT-VIC01/hardware-configuration.nix
+++ systems/SENLPT-VIC01/hardware-configuration.nix
@@ -1,10 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [
-      <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    ];
+#  imports =
+#    [
+#      <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+#    ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ "i915" ];
