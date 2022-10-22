49e9e635467e440ae337a4d95bdb9ed191af4db4
Victor SENE <victor@sene.ovh>
2022-10-22 02:05:08 +0200

    fix configuration invisible for flake

---
+++ systems/SENLPT-VIC01/configuration.nix
@@ -0,0 +1,333 @@
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./users.nix
+      ../common.nix
+    ];
+
+  networking.hostName = "SENLPT-VIC01"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s25.useDHCP = true;
+  networking.interfaces.wlp3s0.useDHCP = true;
+  hardware.bluetooth.enable = true;
+
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  nixpkgs.config = {
+    allowUnfree = false;
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "anydesk" "corefonts" "samsung-unified-linux-driver" "samsung-UnifiedLinuxDriver" "spotify" "spotify-unwrapped" "teams" "zoom"
+    ];
+  };
+  environment.systemPackages = with pkgs; with gnome; with libsForQt5; [
+    anydesk
+    arandr
+    evince
+    filelight
+    file-roller
+    gnome-calculator
+    gnome-disk-utility
+    gwenview
+    jitsi-meet-electron
+    kate
+    kdeconnect-kde
+    kubectl
+    imagemagick
+    libreoffice
+    nautilus
+    nextcloud-client
+    pkgs.networkmanagerapplet
+    okular
+    pavucontrol
+    spectacle
+    texstudio
+    (texlive.combine {
+      inherit (texlive) scheme-small titling collection-langfrench cm-super xargs bigfoot lipsum;
+    })
+    virt-manager
+    virt-viewer
+    win-virtio
+    woeusb
+    xcolor
+    zim
+
+    chromium
+    firefox
+    element-desktop
+    zoom-us
+
+    audacity
+    ffmpeg-full
+    frei0r
+    gimp
+    inkscape
+    kdenlive
+    obs-studio
+    openlpFull
+    qlcplus
+    spotify
+    v4l-utils
+    vlc
+
+    appimage-run
+    gnupg
+    gopass
+    jmtpfs
+    mkpasswd
+    nfs-utils
+    parted
+    youtube-dl
+    xclip
+
+    vitetris
+
+    lxappearance
+    breeze-icons
+    breeze-gtk
+    equilux-theme
+    numix-gtk-theme
+    numix-icon-theme
+    qt5ct
+  ];
+
+  fonts.fonts = with pkgs; [
+    corefonts
+    dejavu_fonts
+    freefont_ttf
+  ];
+
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="1edb", ATTR{idProduct}=="be55", MODE="0666"
+  '';
+
+  programs.wireshark.enable = true;
+  programs.wireshark.package = pkgs.wireshark;
+
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.browserpass.enable = true;
+
+  programs.adb.enable = true;
+
+  services.udev.packages = [ pkgs.qlcplus ];
+
+  #virtualisation.podman.enable = true;
+  virtualisation.kvmgt.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      #runAsRoot = false;
+      package = pkgs.qemu_full;
+      swtpm.enable = true;
+      ovmf.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull ];
+    };
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+  };
+  environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
+
+  services.tlp.enable = true;
+  services.tlp.settings = {
+    START_CHARGE_THRESH_BAT0 = 90;
+    STOP_CHARGE_THRESH_BAT0 = 95;
+    CPU_SCALING_GOVERNOR_ON_AC="performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    CPU_ENERGY_PERF_POLICY_ON_AC="performance";
+    CPU_ENERGY_PERF_POLICY_ON_BAT="balance_power";
+  };
+
+  services.pcscd.enable = true;
+
+  services.fstrim.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ hplip samsung-unified-linux-driver samsung-unified-linux-driver_1_00_37 ];
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  environment.variables = {
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    TERMINAL = "alacritty";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "fr,us";
+    xkbVariant = ",intl";
+    xkbOptions = "grp:win_space_toggle";
+    libinput = {
+      enable = true;
+      touchpad.naturalScrolling = true;
+    };
+    displayManager.defaultSession = "none+i3";
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      configFile = /etc/nixos/misc/i3.config;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+        polybar xss-lock multilockscreen rofi i3-auto-layout
+        rofi-pass rofi-power-menu
+        alacritty
+     ];
+    };
+    desktopManager = {
+      xterm.enable = false;
+    };
+  };
+  services.picom = {
+    enable = true;
+    backend = "glx";
+    vSync = true;
+  };
+  services.autorandr = {
+    enable = true;
+    profiles = {
+      "desk" = {
+        fingerprint = {
+          eDP-1 = "00ffffffffffff0030e4fc030000000000170104951f11780aa3e59659558e271f505400000001010101010101010101010101010101482640a460841a303020250035ae10000019000000000000000000000000000000000000000000fe004c4720446973706c61790a2020000000fe004c503134305744322d54504231002a";
+          HDMI-1 = "00ffffffffffff0030aeab6101010101081d010380331d782e27b5a4574c9f260f5054bdcf00714f8180818c9500b300d1c001010101023a801871382d40582c4500fd1e1100001e000000ff0056333033334c544d0a20202020000000fd00324b1e5311000a202020202020000000fc004c454e20543233692d31300a20017902031ef14b010203040514111213901f230907078301000065030c001000011d007251d01e206e285500fd1e1100001e8c0ad08a20e02d10103e9600fd1e110000188c0ad090204031200c405500fd1e110000180000000000000000000000000000000000000000000000000000000000000000000000000000000000000052";
+          VGA-1 = "00ffffffffffff0030aeab6157464e460e1e01036c331d782e27b5a4574c9f260f5054bdcf00714f8180818c9500b300d1c001010101023a801871382d40582c4500fd1e1100001e000000ff0056333035464e46570a20202020000000fd00324b1e5315000a202020202020000000fc004c454e20543233692d31300a200043";
+        };
+        config = {
+          eDP-1 = {
+            enable = true;
+            mode = "1600x900";
+            position = "0x0";
+            rate = "60.00";
+            crtc = 1;
+            rotate = "normal";
+          };
+          HDMI-1 = {
+            enable = true;
+            mode = "1920x1080";
+            position = "1600x0";
+            rate = "60.00";
+            crtc = 0;
+            primary = true;
+            rotate = "normal";
+          };
+          VGA-1 = {
+            enable = true;
+            mode = "1920x1080";
+            position = "3520x0";
+            rate = "60.00";
+            crtc = 2;
+            rotate = "normal";
+          };
+        };
+        hooks.postswitch = {
+          "update-lockscreen" = "${pkgs.multilockscreen}/bin/multilockscreen --blur 1.0 -u /home/victor/Images/Wallpapers/gears.png";
+        };
+      };
+      "laptop" = {
+        fingerprint = {
+          eDP-1 = "00ffffffffffff0030e4fc030000000000170104951f11780aa3e59659558e271f505400000001010101010101010101010101010101482640a460841a303020250035ae10000019000000000000000000000000000000000000000000fe004c4720446973706c61790a2020000000fe004c503134305744322d54504231002a";
+        };
+        config = {
+          eDP-1 = {
+            enable = true;
+            mode = "1600x900";
+            position = "0x0";
+            rate = "60.00";
+            crtc = 0;
+            rotate = "normal";
+          };
+        };
+        hooks.postswitch = {
+          "update-lockscreen" = "${pkgs.multilockscreen}/bin/multilockscreen --blur 1.0 -u /home/victor/Images/Wallpapers/gears.png";
+        };
+      };
+      "tele" = {
+        fingerprint = {
+          eDP-1 = "00ffffffffffff0030e4fc030000000000170104951f11780aa3e59659558e271f505400000001010101010101010101010101010101482640a460841a303020250035ae10000019000000000000000000000000000000000000000000fe004c4720446973706c61790a2020000000fe004c503134305744322d54504231002a";
+          HDMI-1 = "00ffffffffffff004c2d670b3336333002190103803c22782a9791a556549d250e5054bfef80714f81c0810081809500a9c0b3000101023a801871382d40582c450056502100001e011d007251d01e206e28550056502100001e000000fd00324b1e5111000a202020202020000000fc00533237443339300a2020202020010302031af14690041f130312230907078301000066030c00100080011d00bc52d01e20b828554056502100001e8c0ad090204031200c4055005650210000188c0ad08a20e02d10103e9600565021000018000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000061";
+        };
+        config = {
+          eDP-1 = {
+            enable = true;
+            mode = "1600x900";
+            position = "1920x0";
+            rate = "60.00";
+            crtc = 1;
+            rotate = "normal";
+          };
+          HDMI-1 = {
+            enable = true;
+            mode = "1920x1080";
+            position = "0x0";
+            rate = "60.00";
+            crtc = 0;
+            primary = true;
+            rotate = "normal";
+          };
+        };
+        hooks.postswitch = {
+          "update-lockscreen" = "${pkgs.multilockscreen}/bin/multilockscreen --blur 1.0 -u /home/victor/Images/Wallpapers/gears.png";
+        };
+      };
+    };
+  };
+  environment.etc."i3status.conf".source = /etc/nixos/misc/i3status.config;
+  environment.etc."alacritty.yml".source = /etc/nixos/misc/alacritty.yml;
+
+  programs.dconf.enable = true;
+  services.gnome.evolution-data-server.enable = true;
+  services.gnome.gnome-online-accounts.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.gnome.sushi.enable = true;
+  services.gvfs.enable = true;
+  services.blueman.enable = true;
+  programs.evolution.enable = true;
+  programs.light.enable = true;
+
+  services.fprintd.enable = true;
+  security.pam.services.login.fprintAuth = true;
+  security.pam.services.xscreensaver.fprintAuth = true;
+
+  #services.nginx.enable = true;
+  #services.nginx.virtualHosts = {
+  #  "localhost" = {
+  #    locations."/" = {
+  #     root = "/var/www/";
+  #    };
+  #    default = true;
+  #  };
+  #};
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [
+    1716  # KDEConnect
+    #80 443
+  ];
+  networking.firewall.allowedUDPPorts = [
+    1716  # KDEConnect
+  ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "20.03";
+  system.autoUpgrade.enable = true;
+}
---
+++ systems/SENLPT-VIC03/configuration.nix
@@ -0,0 +1,202 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./users.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+
+  networking.hostName = "SENLPT-VIC03"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.useDHCP = false;
+  # networking.interfaces.enp0s25.useDHCP = true;
+  # networking.interfaces.enp0s26u1u2i6.useDHCP = true;
+  # networking.interfaces.wlo1.useDHCP = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  console.keyMap = "fr";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; with kdeApplications; [
+    ark
+    kate
+    kmail
+    kaddressbook
+    korganizer
+    kdeconnect
+    okular
+    konversation
+    kcalc
+    kdeplasma-addons
+    kdepim-runtime
+    #kdepim-addons
+    akonadiconsole
+    akonadi-calendar
+    akonadi-contacts
+    akonadi-notes
+    spectacle
+    yakuake
+    anydesk
+    mkpasswd
+    
+    nix-index
+    nix-prefetch-scripts
+    nox
+    
+    dnsutils
+    nmap
+    pciutils
+    usbutils
+    htop
+    acpi
+    iperf
+    ncdu
+
+    bat
+    wget
+    tmux
+    neovim
+    git
+    tig
+    tmate
+    jq
+    
+    lm_sensors
+    pdftk
+    ghostscript
+
+    ark
+    kate
+    okular
+    akonadiconsole
+    spectacle
+
+    gwenview
+    imagemagick
+    gnome-breeze
+    arc-theme
+    materia-theme
+    wine-staging
+    wineWowPackages.staging
+    winetricks
+
+    firefox
+    filelight
+    libreoffice
+    gimp
+    vlc
+    appimage-run
+
+    chessx
+    superTux
+    teams
+  ];
+
+  environment.variables = { EDITOR = "nvim"; };
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    terminal = "screen-256color";
+    newSession = true;
+    historyLimit = 10000;
+    extraConfig = ''
+      bind-key a set-window-option synchronize-panes
+      set-option -g mode-keys vi
+      set -g mouse on
+      unbind '|'
+      bind-key % split-window -h
+      unbind '"'
+      bind-key - split-window -v
+      unbind 'n'
+      bind-key n new-window
+    '';
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "docker" "git" "colored-man-pages" "command-not-found" "extract" ];
+    };
+    shellAliases = { ll="ls -alh --color=auto"; dpsa="docker ps -a"; vim="nvim"; };
+    promptInit = ''
+      autoload -U promptinit
+      promptinit
+      prompt adam2
+    '';
+  };
+
+  services.tlp.enable = true;
+  services.tlp.extraConfig = ''
+    DEVICES_TO_DISABLE_ON_STARTUP="bluetooth"
+    START_CHARGE_THRESH_BAT0=85
+    STOP_CHARGE_THRESH_BAT0=90
+    CPU_SCALING_GOVERNOR_ON_BAT=powersave
+    ENERGY_PERF_POLICY_ON_BAT=powersave
+  '';
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ hplip ];
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "fr";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.naturalScrolling = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Open ports in the firewall.
+  #networking.firewall.allowedTCPPorts = [ ];
+  #networking.firewall.allowedUDPPorts = [ ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "20.03";
+  system.autoUpgrade.enable = true;
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 15d";
+}
