5e3377383ee6deb63b16f4392e2d414c7271312a
Victor SENE <victor@sene.ovh>
2022-10-31 10:52:06 +0100

    flake update

--- flake.nix
+++ flake.nix
@@ -18,11 +18,21 @@
 
     supportedSystems = [ "x86_64-linux" ];
 
-    channels.nixpkgs = {
-      config.allowUnfree = true;
-      overlaysBuilder = channels: [
-        (final: prev: { inherit (channels.nixpkgs-unstable) unifi7; })
+    channels = {
+      nixpkgs = {
+        config.allowUnfree = true;
+        overlaysBuilder = channels: [
+          (final: prev: { inherit (channels.nixpkgs-unstable) unifi7; })
+        ];
+      };
+      nixpkgs-unstable.patches = [
+        (nixpkgs-unstable.legacyPackages."x86_64-linux".fetchpatch {
+          name = "pypamtest.patch";
+          url = "https://github.com/NixOS/nixpkgs/pull/198066.patch";
+          sha256 = "sha256-pj+4FgVhCe0YUnI7WKcAvjzNRnEtAhm0cg+/e6xXi7g=";
+        })
       ];
+
     };
 
     hostDefaults.modules = [
