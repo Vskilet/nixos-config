78c1162ae8864ea63daa20b1718c7c1927dfdf5e
Victor SENE <victor@sene.ovh>
2024-01-21 22:13:48 +0100

    SENNAS01: upgrade to 23.11

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "flake:nixpkgs/nixos-23.05";
+    nixpkgs.url = "flake:nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "flake:nixpkgs/nixos-unstable";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.4.0";
     simple-nixos-mailserver = {
       url = "gitlab:simple-nixos-mailserver/nixos-mailserver/master";
       inputs = {
         nixpkgs.follows = "nixpkgs-unstable";
-        nixpkgs-23_05.follows = "nixpkgs";
+        nixpkgs-23_11.follows = "nixpkgs";
       };
     };
     nix-matrix-appservices.url = "gitlab:coffeetables/nix-matrix-appservices";
@@ -20,20 +20,20 @@
     supportedSystems = [ "x86_64-linux" ];
 
     channels = {
-      nixpkgs = {
-        patches = [
-          (nixpkgs.legacyPackages."x86_64-linux".fetchpatch {
-            name = "unifi-7.4.156";
-            url = "https://github.com/NixOS/nixpkgs/commit/756f6cc1ef2da00e1e60ee86286fe6b22f6a5949.patch";
-            sha256 = "sha256-vCmIxWzt7AeC0rdjS90GxsvFEGQ5A7pMj+wKpNhTcQM=";
-          })
-          (nixpkgs.legacyPackages."x86_64-linux".fetchpatch {
-            name = "mautrix-0.19.16";
-            url = "https://github.com/NixOS/nixpkgs/commit/f449215e3850172ae90ae9783051a5a781cb3c87.patch";
-            sha256 = "sha256-VQz4z3bCTb4AiQGCM9bb2hLNclyjEjfjjHrn+FqSn6M=";
-          })
-        ];
-      };
+#      nixpkgs = {
+#        patches = [
+#          (nixpkgs.legacyPackages."x86_64-linux".fetchpatch {
+#            name = "unifi-7.4.156";
+#            url = "https://github.com/NixOS/nixpkgs/commit/756f6cc1ef2da00e1e60ee86286fe6b22f6a5949.patch";
+#            sha256 = "sha256-vCmIxWzt7AeC0rdjS90GxsvFEGQ5A7pMj+wKpNhTcQM=";
+#          })
+#          (nixpkgs.legacyPackages."x86_64-linux".fetchpatch {
+#            name = "mautrix-0.19.16";
+#            url = "https://github.com/NixOS/nixpkgs/commit/f449215e3850172ae90ae9783051a5a781cb3c87.patch";
+#            sha256 = "sha256-VQz4z3bCTb4AiQGCM9bb2hLNclyjEjfjjHrn+FqSn6M=";
+#          })
+#        ];
+#      };
       nixpkgs-unstable = {
         config = {
           allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
