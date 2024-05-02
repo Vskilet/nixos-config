ef9090b49f36c63478fa6569dd0dfdd284ececbe
Victor SENE <victor@sene.ovh>
2024-05-02 21:13:39 +0200

    peertube: 6.0.3 -> 6.0.4

--- services/peertube/default.nix
+++ services/peertube/default.nix
@@ -5,7 +5,7 @@
     backend = "docker";
     containers = {
       peertube = {
-        image = "chocobozzz/peertube:v6.0.3-bookworm";
+        image = "chocobozzz/peertube:v6.0.4-bookworm";
         ports = [ "1935:1935" "9000:9000" ];
         volumes = [
           "/var/lib/peertube/data:/data"
