7545b4c693380644b1c8ea7a065d987d0c87f7fb
Victor SENE <victor@sene.ovh>
2024-07-01 21:53:52 +0200

    nextcloud: upgrade to 29

--- services/nextcloud/default.nix
+++ services/nextcloud/default.nix
@@ -8,7 +8,7 @@
     enable = true;
     hostName = "cloud.sene.ovh";
     https = true;
-    package = pkgs.nextcloud28;
+    package = pkgs.nextcloud29;
     autoUpdateApps.enable = true;
     configureRedis = true;
     config = {
@@ -17,12 +17,17 @@
       dbpassFile = "/mnt/secrets/nextcloud_config_dbpassFile";
       dbtableprefix = "oc_";
       adminpassFile = "/mnt/secrets/nextcloud_config_adminpassFile";
-      extraTrustedDomains = config.services.nginx.virtualHosts."cloud.sene.ovh".serverAliases;
-      defaultPhoneRegion = "FR";
+    };
+    settings = {
+      default_phone_region = "FR";
+#      log_type = "systemd";
+      maintenance_window_start = 1;
+      trusted_domains = config.services.nginx.virtualHosts."cloud.sene.ovh".serverAliases;
     };
     phpOptions = {
       "opcache.interned_strings_buffer" = "10";
     };
+#    phpExtraExtensions = all: [ all.php-systemd ];
   };
   virtualisation.oci-containers = {
     backend = "docker";
