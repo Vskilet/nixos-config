2e1efa44f6b078f26e4097e81ee88be06d3820be
Victor SENE <victor@sene.ovh>
2024-07-04 21:59:07 +0200

    mautrix-signal: use new upstream service with go module

---
+++ services/matrix/signal.nix
@@ -0,0 +1,38 @@
+{ pkgs, config, lib, ... }:
+
+{
+  users.groups.mautrix-signal.members = [ "matrix-synapse" ];
+  services.mautrix-signal = {
+    enable = true;
+    settings = {
+      homeserver.address = config.services.matrix-synapse.settings.public_baseurl;
+      appservice = {
+        bot.displayname = "Signal Bot";
+        database = {
+          type = "sqlite3";
+          uri = "/var/lib/mautrix-signal/database.db";
+        };
+        #ephemeral_events = false;
+        id = "signal";
+      };
+      bridge = {
+        encryption = {
+          allow = true;
+          default = true;
+          require = true;
+        };
+        history_sync = {
+          request_full_sync = true;
+        };
+        mute_bridging = false;
+        permissions = {
+          "@vskilet:sene.ovh" = "admin";
+        };
+        private_chat_portal_meta = true;
+        provisioning = {
+          shared_secret = "disable";
+        };
+      };
+    };
+  };
+}
