00d975fc52a18f34749f0668aedabaf08df9b89d
Victor SENE <victor@sene.ovh>
2024-12-26 20:57:05 +0100

    mautrix-whatsapp: start migration with 0.11.0

--- services/matrix/default.nix
+++ services/matrix/default.nix
@@ -40,7 +40,6 @@
           ];
         }
       ];
-      app_service_config_files = [ "/var/lib/mautrix-whatsapp/whatsapp-registration.yaml" ];
       database_type = "psycopg2";
       database_args = {
         database = "matrix-synapse";
@@ -99,30 +98,33 @@
       homeserver.address = config.services.matrix-synapse.settings.public_baseurl;
       appservice = {
         bot.displayname = "WhatsApp Bot";
-        database = {
-          type = "sqlite3";
-          uri = "/var/lib/mautrix-whatsapp/mautrix-whatsapp.db";
-        };
-        #ephemeral_events = false;
+        ephemeral_events = true;
         id = "whatsapp";
       };
+      backfill.enabled = true;
       bridge = {
-        encryption = {
-          allow = true;
-          default = false;
-          require = false;
-        };
-        history_sync = {
-          request_full_sync = true;
-        };
-        mute_bridging = false;
+        mute_only_on_create = false;
         permissions = {
           "@vskilet:sene.ovh" = "admin";
         };
         private_chat_portal_meta = true;
-        provisioning = {
-          shared_secret = "disable";
-        };
+      };
+      database = {
+        type = "sqlite3";
+        uri = "file:/var/lib/mautrix-whatsapp/mautrix-whatsapp.db?_txlock=immediate";
+      };
+      double_puppet = {
+        servers = {};
+        secrets = {};
+      };
+      encryption = {
+        allow = true;
+        default = false;
+        require = false;
+      };
+      logging.min_level = "debug";
+      provisioning = {
+        shared_secret = "disable";
       };
     };
   };
