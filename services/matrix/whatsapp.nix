d891db771833063ed8767e9aa5da504bdbcf92c4
Victor SENE <victor@sene.ovh>
2025-09-23 09:01:18 +0200

    mautrix-whatsapp: update config (security, name sync, ...)

--- services/matrix/whatsapp.nix
+++ services/matrix/whatsapp.nix
@@ -8,32 +8,38 @@
       homeserver.address = config.services.matrix-synapse.settings.public_baseurl;
       appservice = {
         bot.displayname = "WhatsApp Bot";
+        database = {
+          type = "sqlite3-fk-wal";
+          uri = "file:/var/lib/mautrix-whatsapp/mautrix-whatsapp.db?_txlock=immediate";
+        };
         ephemeral_events = true;
         id = "whatsapp";
       };
       backfill.enabled = true;
       bridge = {
+        bridge_matrix_leave = true;
+        displayname_template = "{{or .FullName .PushName}} (WA)";
+        double_puppet_allow_discovery = true;
+        encryption = {
+          allow = true;
+          default = true;
+          require = false;
+        };
+        history_sync = {
+          backfill = true;
+        };
+        mute_bridging = true;
+        parallel_member_sync = true;
+        private_chat_portal_meta = "always";
+        url_previews = true;
+        user_avatar_sync = true;
         mute_only_on_create = false;
         permissions = {
           "@vskilet:sene.ovh" = "admin";
         };
-        private_chat_portal_meta = true;
-      };
-      database = {
-        type = "sqlite3";
-        uri = "file:/var/lib/mautrix-whatsapp/mautrix-whatsapp.db?_txlock=immediate";
-      };
-      double_puppet = {
-        servers = {};
-        secrets = {};
-      };
-      encryption = {
-        allow = true;
-        default = false;
-        require = false;
-      };
-      provisioning = {
-        shared_secret = "disable";
+        provisioning = {
+          shared_secret = "disable";
+        };
       };
     };
   };
