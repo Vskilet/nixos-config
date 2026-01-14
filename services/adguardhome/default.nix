3580dbd318c26a8dfc316e0b7839166dd7dce0bb
Victor SENE <victor@sene.ovh>
2026-01-14 12:28:58 +0100

    adguardhome: try a new config

--- services/adguardhome/default.nix
+++ services/adguardhome/default.nix
@@ -13,17 +13,34 @@
         bind_hosts = [
           "0.0.0.0"
         ];
-        upstream_dns = [
-          "80.67.169.12"
-          "80.67.169.40"
-          "89.234.141.66"
+        blocked_services = {
+          ids = [
+            "onlyfans"
+          ];
+        };
+        bootstrap_dns = [
+          "86.54.11.11"
+          "86.54.11.211"
+          "2a13:1001::86:54:11:11"
+          "2a13:1001::86:54:11:211"
+        ];
+        fallback_dns = [
+          "https://ns1.fdn.fr/dns-query"
           "1.1.1.1"
         ];
+        upstream_dns = [
+          "https://unfiltered.joindns4.eu/dns-query"
+          "https://ns0.fdn.fr/dns-query"
+          "https://dns.nextdns.io"
+          "89.234.141.66"           # Alsace Reseau Neutre
+          "2a00:5881:8100:1000::3"  # Alsace Reseau Neutre
+        ];
+        upstream_mode = "load_balance";
       };
       filtering = {
         protection_enabled = true;
         filtering_enabled = true;
-        parental_enabled = false;  # Parental control-based DNS requests filtering.
+        parental_enabled = true;  # Parental control-based DNS requests filtering.
         safe_search = {
           enabled = false;  # Enforcing "Safe search" option for search engines, when possible.
         };
@@ -33,6 +50,8 @@
         "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt"   # The Big List of Hacked Malware Web Sites
         "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt"  # malicious url blocklist
       ];
+      language = "fr";
+      theme = "dark";
     };
   };
 
