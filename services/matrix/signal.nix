fc55cad8a25046169f0b27ff52b612ce3cb72ffa
Victor SENE <victor@sene.ovh>
2026-06-01 10:06:02 +0200

    mautrix-signal: use v2 bridge config

--- services/matrix/signal.nix
+++ services/matrix/signal.nix
@@ -7,32 +7,52 @@
     settings = {
       homeserver.address = config.services.matrix-synapse.settings.public_baseurl;
       appservice = {
-        bot.displayname = "Signal Bot";
-        database = {
-          type = "sqlite3";
-          uri = "/var/lib/mautrix-signal/database.db";
+        bot = {
+          username = "signalbot";
+          displayname = "Signal Bot";
         };
-        #ephemeral_events = false;
+        ephemeral_events = true;
         id = "signal";
+        #TO DELETE when service is backported
+        database = lib.mkForce null;
       };
+      backfill.enabled = true;
       bridge = {
-        encryption = {
-          allow = true;
-          default = true;
-          require = false;
-        };
-        history_sync = {
-          request_full_sync = true;
-        };
-        mute_bridging = false;
+        bridge_matrix_leave = true;
+        command_prefix = "!signal";
+        private_chat_portal_meta = true;
+        mute_only_on_create = false;
         permissions = {
+          "*" = "relay";
           "@vskilet:sene.ovh" = "admin";
         };
-        private_chat_portal_meta = true;
-        provisioning = {
-          shared_secret = "disable";
-        };
+        relay.enabled = true;
+        #TO DELETE when service is backported
+        displayname_template = lib.mkForce null;
+        double_puppet_server_map = lib.mkForce null;
+        login_shared_secret_map = lib.mkForce null;
+        username_template = lib.mkForce null;
       };
+      database = {
+        type = "sqlite3-fk-wal";
+        uri = "file:/var/lib/mautrix-signal/database.db?_txlock=immediate";
+      };
+      double_puppet = {
+        allow_discovery = true;
+      };
+      encryption = {
+        allow = true;
+        default = true;
+        require = false;
+      };
+      network = {
+        displayname_template = "{{or .ContactName .ProfileName }} (Signal)";
+        device_name = "mautrix-signal";
+      };
+      provisioning = {
+        shared_secret = "disable";
+      };
+
     };
   };
 }
