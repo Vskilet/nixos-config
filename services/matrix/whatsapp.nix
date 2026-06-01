d04b8c8ac3356b594593486998156995da838e69
Victor SENE <victor@sene.ovh>
2026-06-01 10:07:04 +0200

    mautrix-whatsapp: use v2 bridge config

--- services/matrix/whatsapp.nix
+++ services/matrix/whatsapp.nix
@@ -7,39 +7,51 @@
     settings = {
       homeserver.address = config.services.matrix-synapse.settings.public_baseurl;
       appservice = {
-        bot.displayname = "WhatsApp Bot";
-        database = {
-          type = "sqlite3-fk-wal";
-          uri = "file:/var/lib/mautrix-whatsapp/mautrix-whatsapp.db?_txlock=immediate";
+        bot = {
+          username = "whatsappbot";
+          displayname = "WhatsApp Bot";
         };
         ephemeral_events = true;
         id = "whatsapp";
+        #TO DELETE when service is backported
+        database = lib.mkForce null;
       };
       backfill.enabled = true;
       bridge = {
         bridge_matrix_leave = true;
-        displayname_template = "{{or .FullName .PushName}} (WA)";
-        double_puppet_allow_discovery = true;
-        encryption = {
-          allow = true;
-          default = true;
-          require = false;
-        };
-        history_sync = {
-          backfill = true;
-        };
-        mute_bridging = true;
-        parallel_member_sync = true;
-        private_chat_portal_meta = "always";
-        url_previews = true;
-        user_avatar_sync = true;
+        command_prefix = "!wa";
+        private_chat_portal_meta = true;
         mute_only_on_create = false;
         permissions = {
+          "*" = "relay";
           "@vskilet:sene.ovh" = "admin";
         };
-        provisioning = {
-          shared_secret = "disable";
-        };
+        relay.enabled = true;
+        #TO DELETE when service is backported
+        displayname_template = lib.mkForce null;
+        double_puppet_server_map = lib.mkForce null;
+        login_shared_secret_map = lib.mkForce null;
+        username_template = lib.mkForce null;
+      };
+      database = {
+        type = "sqlite3-fk-wal";
+        uri = "file:/var/lib/mautrix-whatsapp/mautrix-whatsapp.db?_txlock=immediate";
+      };
+      double_puppet = {
+        allow_discovery = true;
+      };
+      encryption = {
+        allow = true;
+        default = true;
+        require = false;
+      };
+      network = {
+        displayname_template = "{{or .FullName .PushName}} (WA)";
+        os_name = "mautrix-whatsapp";
+        url_previews = true;
+      };
+      provisioning = {
+        shared_secret = "disable";
       };
     };
   };
